b4ab4c77fb45fba5ae80241a07e216850d67ee58
benteechur <benkluga@gmail.com>
2019-12-19 11:05:19 -0600

    testing gitignore

--- archive/main.py
+++
@@ -1,8 +0,0 @@
-from functions import save_page
-
-url = 'https://www.niu.edu/publicsafety/emergency/safetybulletin/archive.shtml'
-
-# the entire contents of the page at "url" are now saved in a file named fileName
-fileName = save_page(url)
-
-print(fileName)
