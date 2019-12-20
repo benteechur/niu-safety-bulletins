9319a10dbf9bf2c426bb4ef80cf026f96092b1ec
Rui Zhang <zr9655@outlook.com>
2019-12-19 21:55:25 -0600

    Converted new notification data into df and cleaned

--- parseLive/data_collection.py
+++ parseLive/data_collection.py
@@ -1,6 +1,6 @@
 # Import necessary packages
 
-import requests
+import requests, time, re
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import pandas as pd
@@ -55,6 +55,10 @@ print()
 headers = soup_new.find_all('th')
 print(headers[:])
 
+# Calulate elapsed time and cpu time (start)
+start_elapsed = time.perf_counter()
+start_cpu = time.process_time()
+
 # Put the data that was scraped from web into a dataframe
 safety_lst = []
 row_lst = []
@@ -63,39 +67,83 @@ for row in rows_new:
     row_header = row.find_all('th')
     str_header = str(row_header)
     clean_header = BeautifulSoup(str_header, 'lxml').get_text()
-    print('The cleaned row th is: ', clean_header)
+    splitted_header = re.split('[-,]', clean_header)
+    #print('The splitted header is: ', splitted_header)
+    #print('The cleaned row th is: ', clean_header)
     #print('The length of "*row_header": ', len(*row_header))
-    print()
+    #print()
 
     row_content = row.find_all('td')
     str_content = str(row_content)
     clean_content = BeautifulSoup(str_content, 'lxml').get_text()
-    print('The cleaned row td is: ', clean_content)
+    splitted_content = re.split(r': ,|:,', clean_content)
+    #print('The cleaned row td is: ', clean_content)
     #print('The length of the first row td is: ', len(clean_content))
     #print('The fist element in clean_content:', clean_content[0])
     #print('The data type of row_content: ', type(row_content))
     #print('The data type of str_content: ', type(str_content))
-    print()
+    #print()
 
 
     if len(clean_header) > 2:
-        safety_lst.append(row_lst)
+        #safety_lst.append(row_lst)
+        safety_lst += [row_lst]
         row_lst = []
-        row_lst.append(clean_header)
+        #row_lst.append(clean_header)
+        row_lst += splitted_header
     if len(clean_content) > 2:
-        row_lst.append(clean_content)
-        #print("row_lst inside of the loop is: ", row_lst)
-safety_lst.append(row_lst)  # To append the last element
-
+        #row_lst.append(clean_content)
+        row_lst += splitted_content
     #break
+        #print("row_lst inside of the loop is: ", row_lst)
+#safety_lst.append(row_lst)  # To append the last element
+safety_lst += [row_lst]
+    #break
+
+# Calulate time (end)
+end_elapsed = time.perf_counter()
+end_cpu = time.process_time()
+
 print()
 #print('The list that contains every notification is: ', safety_lst)
 #print('The length of safety_lst is: ', len(safety_lst))
 #print('The last element in the safety_lst: ', safety_lst[-1])
 #print()
 #print()
+print('Elapsed time is: ', end_elapsed - start_elapsed)
+print('CPU time is:     ', end_cpu - start_cpu)
+print()
 
-# Convert the safety_lst into a dataframe
+
+#****************** Convert the safety_lst into a dataframe ******************
+# Set the maximum number of columns so that all the df columns can be shown
+pd.set_option('display.max_columns', 100)
+
+##df_header = ['Incident', 'Date Occurred', 'Location', 'Details',
+##'tips', 'about']
+##df = pd.DataFrame(safety_lst, columns = df_header)
 df = pd.DataFrame(safety_lst)
-df.drop([4, 5], axis = 1, inplace = True)
+#print(df)
+
+# Clean up the DataFrame
+# Dropped the "Safety Tips and Resources" and "About Safety bulletins" columns
+##df.drop(['tips', 'about'], axis = 1, inplace = True)
+##df.drop([0], axis = 0, inplace = True)
+
+# Split columns at comma and dash positions
+##df1 = df
+
+# Drop the last two columns which are "tips and recourse" and "About..."
+df.drop([0, 3, 5, 7, 9, 11, 12], axis = 1, inplace = True)
+df.drop([0], axis = 0, inplace = True)
+
+# Put header into df:
+df_header = ['Notification Type', 'Crime Type', 'On/Off Campus',
+'Date Occurred', 'Location', 'Details']
+df.columns = df_header
+
+# Delete the "]" in 'On/Off Campus', 'Date Occurred', and 'Location' columns
+df['On/Off Campus'] = df['On/Off Campus'].str.rstrip(' ]')
+df['Date Occurred'] = df['Date Occurred'].str.rstrip(' ]')
+df['Location'] = df['Location'].str.rstrip(' ]')
 print(df)
