66db74d9193fa2106622f88601b47426bd3487dc
benteechur <benkluga@gmail.com>
2019-12-26 22:58:56 -0600

    successfully stripping off extra tags from location data

--- archiveData/functions.py
+++ archiveData/functions.py
@@ -15,6 +15,20 @@ datePattern = re.compile(regex2)
 regex3 = r"""(<td valign="top">)(.*)(</td>)"""
 dateDataPattern = re.compile(regex3)
 
+regex4 = r"""<td valign="top"><strong>Location: </strong></td>"""
+locationPattern = re.compile(regex4)
+
+regex5 = r"""(<td valign="top">)(.*)(</td>)"""
+locationDataPattern = re.compile(regex5)
+
+# pattern for stripping off one set of tags
+regex6 = r"""<.*>(.*)</.*>"""
+stripTagPattern1 = re.compile(regex6)
+
+# pattern for stripping off two sets of tags
+regex7 = r"""<.*><.*>(.*)</.*></.*>"""
+stripTagPattern2 = re.compile(regex7)
+
 
 def save_page(x):
 
@@ -43,6 +57,7 @@ def parse_data(line, sentinel):
     incidentTag = incidentPattern.search(line)
     if incidentTag:
         print("found match: ", incidentTag.group(2))
+
     dateTag = datePattern.search(line)
     if dateTag:
         #dateSentinel[0] = True
@@ -56,3 +71,24 @@ def parse_data(line, sentinel):
             print("found date data: ", dateDataTag.group(2))
             #dateSentinel[0] = False
             sentinel["date"] = False
+
+    locationTag = locationPattern.search(line)
+    if locationTag:
+        sentinel["location"] = True
+        return
+    if sentinel["location"]:
+        locationDataTag = locationDataPattern.search(line)
+        if locationDataTag:
+            print("found location data: ", locationDataTag.group(2))
+            temp = locationDataTag.group(2)
+            if "<" in temp:
+                print("extra tags here")
+                locationStripTag = stripTagPattern1.search(temp)
+                # if a group is empty, it is equal to an empty string
+                if locationStripTag.group(1) == "":
+                    locationStripTag = stripTagPattern2.search(temp)
+                    newTemp = locationStripTag.group(1)
+                else:
+                    newTemp = locationStripTag.group(1)
+                print("strippping tags from location data: ", newTemp)
+            sentinel["location"] = False
