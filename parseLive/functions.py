598748548a0a3b3f81cb41050fda133165113386
Rui Zhang <zr9655@outlook.com>
2019-12-29 00:12:44 -0600

    Further cleaned up the dataframe and exported to csv files

--- parseLive/functions.py
+++ parseLive/functions.py
@@ -16,7 +16,8 @@ def process_records(x):
         row_header = row.find_all('th')
         str_header = str(row_header)
         clean_header = BeautifulSoup(str_header, 'lxml').get_text()
-        splitted_header = clean_header.split(',')
+        ##splitted_header = clean_header.split(',')
+        splitted_header = re.split(r'Incident:', clean_header)
 
         row_content = row.find_all('td')
         str_content = str(row_content)
@@ -27,6 +28,7 @@ def process_records(x):
             safety_lst += [row_lst]
             row_lst = []
             row_lst += splitted_header
+            #row_lst += [clean_header]
         if len(clean_content) > 2:
             row_lst += splitted_content
     safety_lst += [row_lst]
@@ -48,24 +50,35 @@ def clean_df(df):
     #print('after dropping: \n', df)
     # Get rid of "]"
     df[1] = df[1].str.rstrip(' ]')
+    df[1] = df[1].str.lstrip(', ')
     df[3] = df[3].str.rstrip(' ]')
     df[5] = df[5].str.rstrip(' ]')
     df[7] = df[7].str.rstrip(' ]')
     # Split the first column
-    df[1] = df[1].str.split(' - ')
-    df.reset_index(inplace = True)
-    df_title_column = pd.DataFrame(df[1].values.tolist())
+    ##df[1] = df[1].str.split(' - ')
+    ##df.reset_index(inplace = True)
+    ##df_title_column = pd.DataFrame(df[1].values.tolist())
     # Concated the splitted first columns with the original df and then dropped
     # the unsplitted first column
-    df_clean = pd.concat([df_title_column, df], axis = 1, ignore_index = True)
-    df_clean.reset_index(inplace = True)
-    df_clean.drop(['index', 2, 4, 5], axis = 1, inplace = True)
+    ##df_clean = pd.concat([df_title_column, df], axis = 1, ignore_index = True)
+    ##df_clean.reset_index(inplace = True)
+    ##df_clean.drop(['index', 2, 4, 5], axis = 1, inplace = True)
     # Convert integer labels into actual names
-    df_header = ['Notification Type', 'Crime Type', 'On/Off Campus',
-    'Date Occurred', 'Location', 'Details']
-    df_clean.columns = df_header
+    ##df_header = ['Notification Type', 'Crime Type', 'On/Off Campus',
+    ##'Date Occurred', 'Location', 'Details']
+    ##df_clean.columns = df_header
 
-    return df_clean
+    ## TEST
+    #df.reset_index(inplace = True)
+    #print('the index column is\n', df['index'])
+
+    #df.reset_index(drop = True, inplace = True)
+    #df.drop(['index'], axis = 1, inplace = True)
+    ##return df_clean
+
+    df_header = ['Incident', 'Date Occurred', 'Location', 'Details']
+    df.columns = df_header
+    return df
 
 # Function:  the function to test nan values in a column
 # Arguments: takes a df and the number of the column that want to be tested
