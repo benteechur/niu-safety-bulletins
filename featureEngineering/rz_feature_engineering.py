dd92038f4ff3f37c57f9776244c30d056f0632f6
Rui Zhang <zr9655@outlook.com>
2020-01-15 13:09:25 -0600

    Rearranged code to work better with Tableau

--- featureEngineering/rz_feature_engineering.py
+++ featureEngineering/rz_feature_engineering.py
@@ -23,31 +23,45 @@ add_loc_map(df)
 
 # Add 'Year', 'Month', 'Day', and 'Dayofweek' columns based on 'Date Occurred'
 # Convert 'Date Occurred' column to datetime date type
-df['Date Occurred'] = pd.to_datetime(df['Date Occurred'])
+##df['Date Occurred'] = pd.to_datetime(df['Date Occurred'])
 # Exact month from 'Date Occurred' column
 # Method 1 (without converting to datetime data type):
 # https://www.interviewqs.com/ddi_code_snippets/extract_month_year_pandas
 #df['Month Occurred'] = pd.DatetimeIndex(df['Date Occurred']).month
 # Method 2 (has to convert to datetime data type):
-df['Year'] = df['Date Occurred'].dt.year
-df['Month'] = df['Date Occurred'].dt.month_name()
-print('\n Month name after extraction:\n', df['Month'].head())
-df['Day'] = df['Date Occurred'].dt.day
+##df['Year'] = df['Date Occurred'].dt.year
+##df['Month'] = df['Date Occurred'].dt.month_name()
+##print('\n Month name after extraction:\n', df['Month'].head())
+##df['Day'] = df['Date Occurred'].dt.day
 # The next line of code only returns the number of week days.
 ##df['Dayofweek'] = df['Date Occurred'].dt.dayofweek
-df['Dayofweek'] = df['Date Occurred'].dt.weekday_name
+##df['Dayofweek'] = df['Date Occurred'].dt.weekday_name
 
 # Add 'Time' column
-extract_time(df, 'Details')
+##extract_time(df, 'Details')
 
 # Add 'Time_24' column
-add_time_24(df)
+##add_time_24(df)
 # Convert values in 'Time_24' column into datetime data type
-df['Time_24'] = pd.to_datetime(df['Time_24'], format = '%H:%M').dt.time
+##df['Time_24'] = pd.to_datetime(df['Time_24'], format = '%H:%M').dt.time
 
 # Rearrange the columns order
-df = df[['Incident', 'Crime Type', 'Date Occurred', 'Year', 'Month', 'Day',
-         'Dayofweek', 'Time_24', 'Location', 'Location_map', 'Details']]
+##df = df[['Incident', 'Crime Type', 'Date Occurred', 'Year', 'Month', 'Day',
+##         'Dayofweek', 'Time_24', 'Location', 'Location_map', 'Details']]
+
+
+
+# Rearrange code to work better in Tableau
+# Add 'Time_24' column
+extract_time(df, 'Details')
+add_time_24(df)
+# Combine date and time to create new datetime type column 'DateTime'
+df['DateTime'] = pd.to_datetime(df['Date Occurred'] + ' ' + df['Time_24'])
+# Convert 'Date Occurred' column to datetime type to create new column 'Dayofweek'
+df['Date Occurred'] = pd.to_datetime(df['Date Occurred'])
+df['Dayofweek'] = df['Date Occurred'].dt.weekday_name
+# Rearrange columns order
+df = df[['Incident', 'Crime Type', 'DateTime', 'Dayofweek', 'Location', 'Location_map', 'Details']]
 
 # Export the dfs after data engineering
 df.to_csv(path + r'\feature_bulletin.csv', index = False, encoding = 'utf-8-sig')
