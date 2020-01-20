106fa8dc43bf446ad2e43512e5c9163151980f98
Rui Zhang <zr9655@outlook.com>
2020-01-20 16:02:35 -0600

    Finalized feature engineering step

--- featureEngineering/rz_feature_engineering.py
+++ featureEngineering/rz_feature_engineering.py
@@ -30,65 +30,33 @@ df['Date Occurred'] = pd.to_datetime(df['Date Occurred'])
 # https://www.interviewqs.com/ddi_code_snippets/extract_month_year_pandas
 #df['Month Occurred'] = pd.DatetimeIndex(df['Date Occurred']).month
 # Method 2 (has to convert to datetime data type):
-#df['Year'] = df['Date Occurred'].dt.year
-#df['Month'] = df['Date Occurred'].dt.month_name()
-#print('\n Month name after extraction:\n', df['Month'].head())
-#df['Day'] = df['Date Occurred'].dt.day
 # The next line of code only returns the number of week days.
 ##df['Dayofweek'] = df['Date Occurred'].dt.dayofweek
-df['Dayofweek'] = df['Date Occurred'].dt.weekday_name #--> don't need it since Tableau will automatically generate
+#--> don't really need it if using Tableau, but I'll keep it here
+df['Dayofweek'] = df['Date Occurred'].dt.weekday_name
 
 # Add 'Time' column
 extract_time(df, 'Details')
 # Add 'Time_24' column
 add_time_24(df)
-# Prepare columns for exporting the csv file which will be used by Google My Map
+
+# Rearrange columns order for exporting the df_mymap.csv file
 df_mymap = df[['Incident', 'Crime Type', 'Date Occurred', 'Time_24', 'Dayofweek',
                'Location_map', 'Details']]
 
 # Convert values in 'Time_24' column into datetime data type
+# (didn't convert it above because don't want to show two seconds digits on
+# Google map)
 df['Time_24'] = pd.to_datetime(df['Time_24']).dt.time
 
-
-# Rearrange the columns order
-##df = df[['Incident', 'Crime Type', 'Date Occurred', 'Year', 'Month', 'Day',
-##         'Dayofweek', 'Time_24', 'Location', 'Location_map', 'Details']]
-
-
-'''
-# Rearrange code for creating time related columns to work better in Tableau
-# --> This appear to not work for the records that don't have a specific time.
-# Add 'Time_24' column
-extract_time(df, 'Details')
-add_time_24(df)
-# Combine date and time to create new datetime type column 'DateTime'
-##df['DateTime'] = [pd.to_datetime(df['Date Occurred'][x]) if pd.isna(df['Time_24'][x])
-##                  else pd.to_datetime(df['Date Occurred'][x] + ' ' + df['Time_24'][x])
-##                  for x in range(0, df.shape[0])]
-df['DateTime'] = pd.to_datetime(df['Date Occurred'] + ' ' + df['Time_24'])
-
-# Convert 'Date Occurred' column to datetime type to create new column 'Dayofweek'
-df['Date Occurred'] = pd.to_datetime(df['Date Occurred'])
-df['Dayofweek'] = df['Date Occurred'].dt.weekday_name
-
-print('\ndf before adding coordinates:\n', df.head(20))
-#print('\nif latitude or longitude in df: ', 'Location_map' in df.columns)
-'''
 # Add 'Latitude' and 'Longitude' columns
-##add_latlng(df)
+add_latlng(df)
 
-#print('\nto check time for cell 36:', df['Time_24'][36])
-
-# Rearrange columns order
-##df = df[['Incident', 'Crime Type', 'DateTime', 'Dayofweek', 'Location',
-##         'Latitude', 'Longitude', 'Details']]
-##df_tableau = df[['Incident', 'Crime Type', 'Date Occurred', 'Time_24', 'Dayofweek',
-##         'Location', 'Location_map', 'Latitude', 'Longitude', 'Details']]
-
-#print('\n The df after adding latlng is: \n', df.head(38))
-
+# Rearrange columns order for exporting df_tableau.csv file
+df_tableau = df[['Incident', 'Crime Type', 'Date Occurred', 'Time_24', 'Dayofweek',
+         'Location', 'Location_map', 'Latitude', 'Longitude', 'Details']]
 
 # Export the dfs after data engineering
-##df_tableau.to_csv(path + r'\feature_bulletin.csv', index = False, encoding = 'utf-8-sig')
+df_tableau.to_csv(path + r'\feature_bulletin.csv', index = False, encoding = 'utf-8-sig')
 df_mymap.to_csv(path + r'\feature_bulletin_mymap.csv', index = False, encoding = 'utf-8-sig')
 print('\nBoth "feature_bulletin_tableau.csv" and "feature_bulletin_mymap.csv" exported successfully!\n')
